4df8001d0d1f26c1851ee1d542b4b096499f6570
kendraliu <120770996+kendraliu@users.noreply.github.com>
2023-06-20 09:49:22 -0700

    update relationships of the tables

--- Pewlett_Hackard_db.sql
+++ Pewlett_Hackard_db.sql
@@ -1,54 +1,69 @@
---drop tables if they already exist
---drop;
+--drop tables in this order if they already exist
+drop table dept_emp;
+drop table dept_manager;
+drop table salaries;
+drop table employees;
+drop table titles;
+drop table departments;
 
---create tables
+--create tables in this order
 create table departments(
-	dept_no varchar,
-	dept_name varchar,
+	dept_no varchar not null, --Once the column is defined as "NOT NULL," it prevents inserting or updating a row with a null value or empty cell in that specific column
+	dept_name varchar not null,
 	primary key (dept_no)
 );
 
-create table dept_emp(
-	emp_no int,
-	dept_no varchar,
-	primary key (emp_no),
-	foreign key (dept_no) references departments(dept_no)
-);
-
 create table titles(
-	title_id varchar,
-	title varchar,
+	title_id varchar not null,
+	title varchar not null,
 	primary key (title_id)
 );
 
-create table dept_manager(
-	dept_no varchar,
-	emp_no int,
-	foreign key (dept_no) references departments(dept_no)
-);
-
 create table employees(
-	emp_no int,
-	emp_title_id varchar,
-	birth_date timestamp,
-	first_name text, 
-	last_name text,
-	sex varchar,
-	hire_date timestamp,
-	foreign key (emp_no) references dept_emp(emp_no),
+	emp_no int not null,
+	emp_title_id varchar not null,
+	birth_date date not null,
+	first_name text not null, 
+	last_name text not null,
+	sex varchar not null,
+	hire_date date not null,
+	primary key (emp_no),
 	foreign key (emp_title_id) references titles(title_id)
 );
 
-create table salaries(
-	emp_no int,
-	salary int,
-	foreign key (emp_no) references dept_emp(emp_no)
+create table dept_manager(
+	dept_no varchar not null,
+	emp_no int not null,
+	primary key (dept_no, emp_no),
+	foreign key (dept_no) references departments(dept_no),
+	foreign key (emp_no) references employees(emp_no)
+);
+
+create table dept_emp(
+	emp_no int not null,
+	dept_no varchar not null,
+	primary key (emp_no, dept_no),
+	foreign key (dept_no) references departments(dept_no),
+	foreign key (emp_no) references employees(emp_no)
 );
 
 
+create table salaries(
+	emp_no int not null,
+	salary int not null,
+	primary key (emp_no),
+	foreign key (emp_no) references employees(emp_no)
+);
+
+--confirm the tables are imported successfully
 select * from departments;
-select * from dept_emp;
 select * from titles;
-select * from dept_manager;
 select * from employees;
+select * from dept_manager;
+select * from dept_emp;
 select * from salaries;
+
+--
+
+
+
